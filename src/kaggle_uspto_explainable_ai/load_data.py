901b5edb8cab54dc3d7421ea128fc0faf3c25b6e
osushinekotan <spmt3710@gmail.com>
2024-10-28 21:00:35 +0900

    Add function to download additional Kaggle datasets whoose utils

--- src/kaggle_uspto_explainable_ai/load_data.py
+++ src/kaggle_uspto_explainable_ai/load_data.py
@@ -1,6 +1,9 @@
 import kaggle
 
-from kaggle_uspto_explainable_ai.kaggle_utils.dataset import download_kaggle_competition_dataset
+from kaggle_uspto_explainable_ai.kaggle_utils.dataset import (
+    download_kaggle_competition_dataset,
+    download_kaggle_datasets,
+)
 
 kaggle_client = kaggle.KaggleApi()
 kaggle_client.authenticate()
@@ -10,6 +13,13 @@ def main() -> None:
     download_kaggle_competition_dataset(
         client=kaggle_client,
         competition="uspto-explainable-ai",
+        out_dir="data/inputs",
+        force=False,
+    )
+
+    download_kaggle_datasets(
+        client=kaggle_client,
+        datasets=["metric/whoosh-wheel-2-7-4"],
         out_dir="data/inputs",
         force=False,
     )
