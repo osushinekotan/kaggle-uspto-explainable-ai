f7e8cd5f2499dabee36d5b84580fa28d2728821e
osushinekotan <spmt3710@gmail.com>
2024-10-29 22:14:55 +0900

    Add function to download additional Kaggle datasets and update SQLite database creation script

--- src/kaggle_uspto_explainable_ai/create_db.py
+++ src/kaggle_uspto_explainable_ai/create_db.py
@@ -1,5 +1,9 @@
+import sqlite3
+from itertools import chain
+
 import polars as pl
 import rootutils
+from loguru import logger
 from tqdm import tqdm
 
 ROOT_DIR = rootutils.setup_root(".", cwd=True)
@@ -8,10 +12,24 @@ DATA_DIR = ROOT_DIR / "data"
 INPUT_DIR = DATA_DIR / "inputs"
 COMPETITION_DIR = INPUT_DIR / "uspto-explainable-ai"
 
-SQLITE_PATH = f"sqlite:///{(COMPETITION_DIR / 'uspto.db').as_posix()}"
+SQLITE_PATH = INPUT_DIR / "db" / "uspto.db"
+SQLITE_PATH.parent.mkdir(parents=True, exist_ok=True)
+
+TABLE_NAME = "patent_data"
 
 
 def main() -> None:
+    conn = sqlite3.connect(SQLITE_PATH)
+    try:
+        existing_publication_numbers = list(
+            chain.from_iterable(set(conn.execute("SELECT DISTINCT publication_number FROM patent_data").fetchall()))
+        )
+        conn.close()
+
+    except sqlite3.OperationalError:
+        existing_publication_numbers = []
+    logger.info(f"# of existing year-month pairs: {len(existing_publication_numbers)}")
+
     patent_metadata_df = (
         pl.scan_parquet(COMPETITION_DIR / "patent_metadata.parquet")
         .filter(pl.col("publication_date") >= pl.date(1975, 1, 1))
@@ -27,16 +45,22 @@ def main() -> None:
         patent_metadata_df.group_by(["year", "month"]),
         total=patent_metadata_df.select(["year", "month"]).n_unique(),
     ):
+        conn = sqlite3.connect(SQLITE_PATH)
         patent_path = COMPETITION_DIR / f"patent_data/{year}_{month}.parquet"
-        patent_df = pl.scan_parquet(patent_path).select(pl.exclude(["claims", "description"]))
-
-        # https://docs.pola.rs/api/python/dev/reference/api/polars.DataFrame.write_database.html
-        patent_df.write_database(
-            table_name="patent_data",
-            connection=SQLITE_PATH,
-            engine="abbc",
-            if_table_exists="append",
+        _ = (
+            pl.scan_parquet(patent_path)
+            .filter(pl.col("publication_number").is_in(existing_publication_numbers).not_())
+            .collect()
+            .to_pandas()
+            .to_sql(
+                name=TABLE_NAME,
+                con=conn,
+                if_exists="append",
+                index=False,
+                chunksize=500,
+            )
         )
+        conn.close()
 
 
 if __name__ == "__main__":
